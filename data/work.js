b2d938280c5144caa9a759d0af02c8dc707052be
haayd <haydee.sramirez@gmail.com>
2022-06-06 13:43:37 -0700

    add html title name

---
+++ data/work.js
@@ -0,0 +1,19 @@
+//
+// Image
+// Title
+// Description
+// GitHub Link
+// Deployment Link
+
+
+const projects = [
+  {
+  title: "Tic-Tac-Toe",
+  imgae: "",
+  description:"how the game works...", 
+  githubLink: "", 
+  deploymentLink: ''
+
+  }
+
+]
---
+++ js/app.js
@@ -0,0 +1,36 @@
+// 1.) add the HTML buttons for linked in, GitHub, email, resume, 
+// 2.) add a container element for bio component to append to 
+// 3.) container element for projects (TTT) will sit 
+// 4.) add event listener to each of my buttons 
+// 5.) write and export functions to access data info 
+// 6.) make sure your functions work as you intended them to work! ( always reference bootstrap and Data!) 
+// 7.) get you favicon inserted to your file 
+// 8.) create a bouncy bounce bootstrap for your icon buttons stated above. 
+// 9.) add an x option so they can exit out when they click on your page 
+// 10.) when they click the ex remove current information they are seeing. 
+// 11.) add a responsive design 
+// 12.)Add google fonts + favicon things 
+// 13.) make sure it is mobile friendly reference previous lectures.
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+const projects = []
+
+/*------------------------ Cached Element References ------------------------*/
+const linkedInIcon = document.querySelector("#linkedIn")
+const githubLinkIcon = document.querySelector("#githubIcon")
+const emailIcon = document.querySelector("#emailIcon")
+const resumeIcon = document.querySelector("#resumeIcon")
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
